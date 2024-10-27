da08dbfab1f266e7f33758b9e75ea69c3694d7d7
Stephen Sanchez <stephensanchez@Stephens-MacBook-Pro.local>
2024-10-27 15:10:54 -0500

    Fix up the report limit

--- reports.py
+++ reports.py
@@ -4,6 +4,8 @@ from datetime import datetime
 from chicago import Crime, School
 from chicago import pull_schools, pull_crimes, degrees_within_miles
 
+CRIME_LIMIT = 3000
+
 ISO_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
 
 def crimes_near_schools(file_name):
@@ -13,7 +15,7 @@ def crimes_near_schools(file_name):
     crimes = collect_all_crimes()
 
     with open(file_name, "w") as report:
-        writer = csv.writer(report, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        writer = csv.writer(report, delimiter=',')
         writer.writerow(["School Name", "School Address", "Crime ID", "Crime Latitude", "Crime Longitude"])
         for school in schools:
             found_crimes = 0
@@ -29,14 +31,14 @@ def collect_all_crimes():
     # `while True` is an infinite loop, because "True" is always True. It is important that we `break` a few lines down on a specific condition.
     while True:
         new_crimes = pull_crimes(1000, len(crimes))
-        if crimes:
-            print(f"Latest crime ID is {crimes[-1].crime_id} and new crime ID is {new_crimes[0].crime_id}")
-            print(f"Latest crime date is {crimes[-1].date} and new crime date is {new_crimes[0].date}")
         if crimes and compare_string_dates(crimes[-1].date, new_crimes[0].date):
             # This API loops around to the first crimes after we've seen them all. Break when we see the same crimes again.
             break
         crimes.extend(new_crimes)
         print(f"Collected {len(crimes)} crimes")
+        if len(crimes) >= CRIME_LIMIT:
+            print("Collected enough crimes, raise CRIME_LIMIT if you want more.")
+            break
     return crimes
 
 def compare_string_dates(date_str1, date_str2, date_format=ISO_FORMAT):
